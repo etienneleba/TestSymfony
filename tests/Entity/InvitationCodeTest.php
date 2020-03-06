bbb0929f93f518fadac85cc0cd431c88f265c490
etienneleba <etienne@lebarillier.fr>
2020-03-06 14:00:18 +0100

    tests first

---
+++ tests/Entity/InvitationCodeTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\InvitationCode;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class InvitationCodeTest extends KernelTestCase
+{
+    use FixturesTrait;
+
+    public function getEntity(): InvitationCode
+    {
+        return $code = (new InvitationCode())
+            ->setCode('12345')
+            ->setDescription('Description de test')
+            ->setExpireAt(new \DateTime())
+        ;
+    }
+
+    public function assertAsErrors(InvitationCode $code, int $number = 0)
+    {
+        self::bootKernel();
+        $error = self::$container->get('validator')->validate($code);
+        // pour compter le nombre d'erreur on utilise assertCount
+        $this->assertCount($number, $error);
+    }
+
+    public function testValidCodeEntity()
+    {
+        $this->assertAsErrors($this->getEntity(), 0);
+    }
+
+    public function testInvalidCodeEntity()
+    {
+        $this->assertAsErrors($this->getEntity()->setCode('1a345'), 1);
+        $this->assertAsErrors($this->getEntity()->setCode('1a34'), 1);
+    }
+
+    public function testInvalidBlankCodeEntity()
+    {
+        $this->assertAsErrors($this->getEntity()->setCode(''), 1);
+    }
+
+    public function testInvalidBlankDescriptionEntity()
+    {
+        $this->assertAsErrors($this->getEntity()->setDescription(''), 1);
+    }
+
+    public function testInvalidUsedCodeEntity()
+    {
+        $this->loadFixtureFiles([__DIR__.'/InvitationCodeTestFixtures.yaml']);
+        $this->assertAsErrors($this->getEntity()->setCode('54321'), 1);
+    }
+}
